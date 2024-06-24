7a7b1f20cd2d59be9bd55b197b72404221dc5c31
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-06-24 21:59:21 +0200

    fixxx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import "./index.css";
 
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate> */}
   </Provider>
 );
