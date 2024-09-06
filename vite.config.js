97f5ccdb21788d689e59bfb393cdc4f43b73e230
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-09-06 22:35:27 +0200

    fixx

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://connections-api.herokuapp.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });
